d8c2e98e6fa10eb21952ce6eb5419c3c6f765ada
Hung-Son Nguyen-Dinh <91714890+sonhung3200@users.noreply.github.com>
2025-03-11 03:33:27 +0700

    Update configs.py

--- src/configs.py
+++ src/configs.py
@@ -1,8 +1,17 @@
+# config.py
+
 best_bpsp = float("inf")
 n_feats = 64
 scale = 3
 resblocks = 3
 K = 10
 plot = ""
+
+# Bật log_likelihood để tính log xác suất
 log_likelihood = True
-collect_probs = True
+
+# Bật collect_probs để thu thập xác suất của dữ liệu huấn luyện
+collect_probs = True  
+
+# Đường dẫn file lưu xác suất
+prob_save_path = "train_probs.json"
