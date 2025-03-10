bc1fa898b94df0f26a2885df2271ddaf7888f1b7
Hung-Son Nguyen-Dinh <91714890+sonhung3200@users.noreply.github.com>
2025-03-11 04:51:50 +0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -38,8 +38,8 @@ def save_collect_probs(probs, train_iter, save_path="collect_probs.json"):
                 "values": y_i.tolist()
             })
 
-    # Giới hạn số lượng collect_probs lưu lại (chỉ lấy 100 phần tử đầu tiên)
-    formatted_probs = formatted_probs[:100]
+    # Chỉ lấy 10 phần tử cuối cùng
+    formatted_probs = formatted_probs[-10:]
 
     save_data = {
         "train_iter": train_iter,
@@ -56,6 +56,7 @@ def save_collect_probs(probs, train_iter, save_path="collect_probs.json"):
 
     with open(save_path, "w") as file:
         json.dump(existing_data, file, indent=4)
+
 
 
 
