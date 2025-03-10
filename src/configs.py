024bf13ec5226d9f07b61d284328eaef3a173f0f
Hung-Son Nguyen-Dinh <91714890+sonhung3200@users.noreply.github.com>
2025-03-11 03:28:49 +0700

    Update configs.py

--- src/configs.py
+++ src/configs.py
@@ -1,10 +1,8 @@
-collect_probs = True  # Bật thu thập xác suất
-log_likelihood = True  # Bật log-likelihood để tính toán loss
-
-n_feats = 64  # Số lượng features
-resblocks = 5  # Số lượng ResNet blocks
-K = 10  # Số cụm trong mô hình logistic mixture
-scale = 3  # Scale downsampling
-
-plot = "./logs"  # Đường dẫn lưu tensorboard
-best_bpsp = float("inf")  # Giá trị BPSP tốt nhất
+best_bpsp = float("inf")
+n_feats = 64
+scale = 3
+resblocks = 3
+K = 10
+plot = ""
+log_likelihood = True
+collect_probs = True
